6ddf19cb0b29c20634bf47b7b6addb55fa26f9db
joaovictorguedes <joao.guedes@webmotors.com.br>
2024-11-16 14:08:31 -0300

    feat: background color in homeView

--- EntreLinhasIOS/Sources/Classes/HomeView/HomeView.swift
+++ EntreLinhasIOS/Sources/Classes/HomeView/HomeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home View")
+        VStack {
+            Text("Home View")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(EntreLinhasIOSAsset.Assets.ashGray.swiftUIColor)
     }
 }
 
