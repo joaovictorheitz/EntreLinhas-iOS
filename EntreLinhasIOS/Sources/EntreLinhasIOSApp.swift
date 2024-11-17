aa9e56b6fec9b98b1502b37cb20c8d708f4636c4
joaovictorguedes <joao.guedes@webmotors.com.br>
2024-11-17 14:43:44 -0300

    style: remove code

--- EntreLinhasIOS/Sources/EntreLinhasIOSApp.swift
+++ EntreLinhasIOS/Sources/EntreLinhasIOSApp.swift
@@ -2,22 +2,6 @@ import SwiftUI
 
 @main
 struct EntreLinhasIOSApp: App {
-    let supabaseManager = SupabaseManager.shared
-    
-    var screen: Screen = .login
-    
-    init() {
-        guard let jwt = supabaseManager.loadJWT() else { return }
-        
-        do {
-            try supabaseManager.retrieveSession(jwt: jwt)
-            
-            screen = .home
-        } catch {
-            screen = .login
-        }
-    }
-    
     var body: some Scene {
         WindowGroup {
             CoordinatorView()
